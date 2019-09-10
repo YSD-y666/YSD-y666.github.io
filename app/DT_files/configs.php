0e8119fc32541124f5af66fe09db60bcf00b1379
yaoshuaiding <2553402391@qq.com>
2019-09-10 19:10:44 +0800

    ap

---
+++ app/DT_files/configs.php
@@ -0,0 +1,1 @@
+Piwik.HeatmapSessionRecording.configuration.assign({"heatmaps":[],"sessions":[{"id":32,"sample_rate":"10.0","min_time":5,"activity":true,"keystrokes":true}],"idsite":"1","trackerid":"HYL2Jz"});
